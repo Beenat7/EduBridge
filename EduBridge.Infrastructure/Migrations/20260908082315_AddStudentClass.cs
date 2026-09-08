b0242433131baf7fe77407ffe191ce0b28098f9d
Beenatlay <beenatlaynigussie910@gmail.com>
2026-09-08 12:44:07 +0300

    feat(student): add student class relationship migration

---
+++ EduBridge.Infrastructure/Migrations/20260908082315_AddStudentClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduBridge.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStudentClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ClassId",
+                table: "Students",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_ClassId",
+                table: "Students",
+                column: "ClassId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Classes_ClassId",
+                table: "Students",
+                column: "ClassId",
+                principalTable: "Classes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Classes_ClassId",
+                table: "Students");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Students_ClassId",
+                table: "Students");
+
+            migrationBuilder.DropColumn(
+                name: "ClassId",
+                table: "Students");
+        }
+    }
+}
